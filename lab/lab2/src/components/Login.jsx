f766b0a39443709471fc4a684fdd23e3fbfae60b
timani2 <tanphong0730@gmail.com>
2026-01-15 11:40:19 +0700

    hoan thanh lab2

--- lab/lab2/src/components/Login.jsx
+++ lab/lab2/src/components/Login.jsx
@@ -2,38 +2,35 @@
 // login page using userName and password to login and redirect to Orchid page
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {
-  Container,
-  Row,
-  Col,
-  Card,
-  Form,
-  Button,
-  Alert,
-} from "react-bootstrap";
+import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+  const [validated, setValidated] = useState(false);
   const navigate = useNavigate();
   const { login } = useAuth();
 
   const handleLogin = (e) => {
+    const form = e.currentTarget;
     e.preventDefault();
+    if (form.checkValidity() === false) {
+      e.stopPropagation();
+      setValidated(true);
+      return;
+    }
     if (username === "admin" && password === "123456") {
       login(username);
       navigate("/");
     } else {
-      setErrorMessage("Invalid username or password");
+      alert("Invalid username or password");
     }
   };
 
   const handleCancel = () => {
     setUsername("");
     setPassword("");
-    setErrorMessage("");
     navigate("/");
   };
   return (
@@ -45,29 +42,33 @@ const Login = () => {
               <h3>Login</h3>
             </Card.Header>
             <Card.Body>
-              {errorMessage && <Alert variant="danger">{errorMessage}</Alert>}
-
-              <Form onSubmit={handleLogin}>
+              <Form noValidate validated={validated} onSubmit={handleLogin}>
                 <Form.Group className="mb-3" controlId="formBasicUsername">
                   <Form.Label>Username</Form.Label>
                   <Form.Control
+                    required
                     type="text"
                     placeholder="Enter username"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
-                    required
                   />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide a username.
+                  </Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                   <Form.Label>Password</Form.Label>
                   <Form.Control
+                    required
                     type="password"
                     placeholder="Password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    required
                   />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide a password.
+                  </Form.Control.Feedback>
                 </Form.Group>
 
                 <div className="d-flex justify-content-center gap-3">
